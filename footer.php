99b1c9b2024474e3a06a3cc805a431b75ad19ef3
cramdesign <mcram@Cram-SSU-iMac.local>
2015-05-05 13:39:40 -0400

    updates

--- footer.php
+++ footer.php
@@ -2,10 +2,12 @@
 
 <?php if( function_exists( 'simple_breadcrumb' ) ) simple_breadcrumb(); ?>
 
+<?php $options = get_option('cram_options'); ?>
+
 <div id="footer">
 <div class="row">
 
-	<h4><?php echo get_option('cram_options')['footer_text']; ?></h4>
+	<h4><?php echo $options['footer_text']; ?></h4>
 
     <nav><?php if ( has_nav_menu( 'social' ) ) wp_nav_menu( array( 'menu' => 'Social' ) ); ?></nav>
 
